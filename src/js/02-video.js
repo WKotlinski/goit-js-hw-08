3fb537ad7c8fb623595d1efb2f2e97e758302711
WKotlinski <132941585+WKotlinski@users.noreply.github.com>
2023-11-19 20:18:32 +0100

    HW

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
-
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const video = document.querySelector('#vimeo-player');
+const player = new Player(video);
+player.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  }, 500)
+);
+window.addEventListener('DOMContentLoaded', () => {
+  const timer = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(timer);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const msg = document.querySelector('textarea[name=message]');
 
+const saveDate = function () {
+  const values = {
+    email: email.value,
+    message: msg.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(values));
+};
+const getValues = function () {
+  const stringValues = localStorage.getItem('feedback-form-state');
+  const correctValues = JSON.parse(stringValues);
+  email.value = correctValues.email;
+  msg.value = correctValues.message;
+};
+
+const clearFun = function (event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  email.value = '';
+  msg.value = '';
+};
+form.addEventListener('input', throttle(saveDate, 1000));
+form.addEventListener('submit', throttle(clearFun, 1000));
+window.addEventListener('DOMContentLoaded', getValues);
